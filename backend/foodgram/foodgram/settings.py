62269274062397522bbf343512173a07be500f34
Роман Недосеко <freelancerroma@mail.ru>
2023-12-21 11:02:03 +0300

    CSRF

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -14,7 +14,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(' ')
 
-CSRF_TRUSTED_ORIGINS = ['https://foodgram-yaroma.ddns.net']
+CSRF_TRUSTED_ORIGINS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
